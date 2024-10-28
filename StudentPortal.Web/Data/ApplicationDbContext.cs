a6abc933acb0f085883127872f82a653a04733cd
StaticSheeps <AnthonySaavedra@protonmail.com>
2024-10-28 23:32:25 +0800

    Added Subject and Schedule Entities

--- StudentPortal.Web/Data/ApplicationDbContext.cs
+++ StudentPortal.Web/Data/ApplicationDbContext.cs
@@ -10,5 +10,7 @@ namespace StudentPortal.Web.Data
         }
 
         public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
     }
 }
